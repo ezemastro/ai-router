c793e7271f6937277cc4619c217df76fba5e424e
ezemastro <mastropietroezequiel@gmail.com>
2026-05-02 23:22:17 +0000

    Agregar servicio groq para manejo de chat utilizando la SDK de Groq

---
+++ server/services/groq.ts
@@ -0,0 +1,26 @@
+import { Groq } from 'groq-sdk';
+import type { AIService, ChatMessage } from '../types';
+
+const groq = new Groq();
+
+export const groqService: AIService = {
+  name: 'Groq',
+  async chat(messages: ChatMessage[]) {
+    const chatCompletion = await groq.chat.completions.create({
+      messages,
+      "model": "llama-3.3-70b-versatile",
+      "temperature": 1,
+      "max_completion_tokens": 1024,
+      "top_p": 1,
+      "stream": true,
+      "stop": null
+    });
+    
+    return (async function*() {
+      for await (const chunk of chatCompletion) {
+        yield chunk.choices[0]?.delta?.content || ''
+      }
+    })();
+  }
+}
+
